3dd4e02f0ce9b4ffa4b6081dfedb28971d2b9742
Malheiros76 <l_souza.malheiros@hotmail.com>
2025-07-04 16:30:13 -0300

    Update apponline.py

--- apponline.py
+++ apponline.py
@@ -489,7 +489,7 @@ def pagina_mensalidades():
             st.markdown(f"📌 {m['aluno']} | Vencimento: {m['vencimento']} | Pago: {pago}")
     else:
         st.info("Nenhuma mensalidade registrada.")
-        st.header("🥋 空手道 (Karatedō) - Registrar Mensalidade")
+    st.header("🥋 空手道 (Karatedō) - Registrar Mensalidade")
         with st.form("form_mensalidade"):
             alunos = list(col_alunos.find())
             aluno_nomes = [a["nome"] for a in alunos]
